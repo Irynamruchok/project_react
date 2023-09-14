b58553e1c4ffa5b9668a5d62b8d79e264f62657b
Irynamruchok <mruchokira@gmail.com>
2023-09-14 16:01:12 +0300

    project

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -3,34 +3,46 @@ import React, {useEffect} from 'react';
 import MoviesListCard from "../MoviesListCard/MoviesListCard";
 import {moviesActions} from "../../redux/slices/moviesSlice";
 import {useDispatch, useSelector} from "react-redux";
+import {useSearchParams} from "react-router-dom";
 
 
 const MoviesList = () => {
-    const {movies, currentPage} = useSelector((state) => state.movies);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const currentPage = parseInt(searchParams.get('page')) || 1
+
+
+    const {movies} = useSelector(state => state.movies);
 
     const dispatch = useDispatch();
 
     useEffect(() =>{
         console.log("Component updated with currentPage:", currentPage);
-        dispatch(moviesActions.getAll(currentPage))
-    },[dispatch,currentPage])
+        dispatch(moviesActions.getAll())
+    },[currentPage])
 
-    const getPrevPage = () => {
-      if (currentPage > 1) {
-          const prevPage = currentPage -1;
-          dispatch(moviesActions.getCurrentPage(prevPage))
-      }
-    }
     const getNextPage = () => {
-        // const nextPage = currentPage + 1;
-        dispatch(moviesActions.getCurrentPage(currentPage +1))
-    }
+        const nextPage = currentPage + 1;
+        setSearchParams({ page: nextPage.toString() });
+        dispatch(moviesActions.getAll(nextPage));
+    };
+
+        const getPrevPage = () => {
+    { if (currentPage > 1) {
+        setSearchParams({ page: (currentPage - 1).toString() });
+    }}  }
+    // }
+    // const getNextPage = () => {
+    //     const nextPage = currentPage + 1;
+    //     console.log(nextPage)
+    //     dispatch(moviesActions.getCurrentPage(nextPage))
+    // }
 
     return (
         <div>
             {movies.map((movie)=> <MoviesListCard key={movie.id} movie={movie}/>)}
             <div>
-                <button onClick={getPrevPage} disabled={currentPage === 1}>Prev</button>
+                <button onClick={getPrevPage}disabled={currentPage === 1}>Prev</button>
                 <button onClick={getNextPage}>Next</button>
             </div>
         </div>
